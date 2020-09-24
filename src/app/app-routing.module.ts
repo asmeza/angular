08f4f454bc00c4f08dce311c1dad32bb4d192b0c
Alan Meza <asmeza@uniguajira.edu.co>
2020-09-23 22:01:11 -0500

    angular primera parte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//imporamos el componente del carrusel que queremos usar como ventana principal 
+import { CarruselComponent } from './components/layouts/carrusel/carrusel.component';
+
+//imporamos el componente del usuario
+import { FormUserComponent } from './components/user/form-user/form-user.component';
+
+const routes: Routes = [
+  
+  //primero indicamos la  ruta inicial de nuetro proyecto 
+  {
+    path: '', 
+    redirectTo: 'home', // le indicamos que cuando nuestro proyecto inicie iniciara en la ruta home
+    pathMatch: 'full'
+  },
+
+  //indicamos la ruta home, es decir cuando en el navegador se ejecute la ruta home
+  //me mostrara el contenido de component del carrusel.
+  {
+    path: 'home',
+    component:CarruselComponent
+  },
+  //creamos nuestra ruta del componente usuario
+  {
+    path: 'user',
+    component: FormUserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/layouts/menu/menu.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { CarruselComponent } from './components/layouts/carrusel/carrusel.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { FormUserComponent } from './components/user/form-user/form-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    CarruselComponent,
+    FooterComponent,
+    FormUserComponent
   ],
   imports: [
     BrowserModule,
